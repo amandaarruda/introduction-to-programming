421fd8a5ab7745a0b23c49f02d363de0eec1d80a
Amanda Arruda <66084295+amandaarruda@users.noreply.github.com>
2022-07-07 21:21:55 -0300

    nova solução

--- ticks.py
+++ ticks.py
@@ -2,12 +2,16 @@
 D = int(input())
 C = int(input())
 
-#ticks a cada 10 min no jogo
-ticks_dia = 12000
+#ticks a cada 1 min no jogo
+ticks_min = 1200
 
-#tempo de jogo por dia
+#o tempo de jogo por dia é de 180 min, mas ele só constroi durante 90 min
+#tempo total de jogo
+tempo_total = 90 * D
 
+#minutos gastos em cada casa
+tempo_casa = tempo_total / C
 
 #Quantidade de ticks gasta em cada casa
-total_ticks = D*C
-print(total_ticks)
+total_ticks = tempo_casa * ticks_min
+print(int(total_ticks))
