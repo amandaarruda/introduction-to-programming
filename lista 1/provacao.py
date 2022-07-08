928d12bdfd2c8cd39e64acb61834349b38d8b159
Amanda Arruda <66084295+amandaarruda@users.noreply.github.com>
2022-07-08 17:58:05 -0300

    Create provacao.py

---
+++ lista 1/provacao.py
@@ -0,0 +1,16 @@
+#Fala do narrador
+S = input()
+
+#Ações
+if (S == 'Olha a chuva!'):
+    print('Grite choveu!')
+elif (S == 'Olha a cobra!'):
+    print('Grite é mentira')
+elif (S == 'Cavalheiros ajoelhados de frente pras damas!'):
+    print('Ajoelhe-se na frente da dama, beije a mão dela e grite beijou!')
+elif (S == 'Alavantu!'):
+    print('Vá em direção a sua dama')
+elif (S == 'Anarriê!'):
+    print('Se distancie da sua dama')
+elif (S == 'Balancê!'): #não coloquei o else para que outros inputs não deem esse resultado
+    print('Dance com a sua dama')
