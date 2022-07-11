3a656c46f4b7ea5d73cba7b3dcdbb1b2ab0c5b68
Amanda Arruda <66084295+amandaarruda@users.noreply.github.com>
2022-07-11 17:40:59 -0300

    Update fogueira.py

--- lista 1/fogueira.py
+++ lista 1/fogueira.py
@@ -1,4 +1,4 @@
-#Entradas
+##Entradas
 nome_1 = input()
 pontuacao_1 = int(input())
 nome_2 = input()
@@ -51,4 +51,108 @@ else:
     pontuacao_primeiro = pontuacao_3
 
 #Segundo lugar
-if 
+if (pontuacao_1 < pontuacao_2 and pontuacao_1 > pontuacao_3):
+    pontuacao_segundo = pontuacao_1
+    nome_segundo = nome_1
+elif (pontuacao_1 < pontuacao_3 and pontuacao_1 > pontuacao_2):
+    pontuacao_segundo = pontuacao_1
+    nome_segundo = nome_1
+elif (pontuacao_2 < pontuacao_1 and pontuacao_2 > pontuacao_3):
+    pontuacao_segundo = pontuacao_2
+    nome_segundo = nome_2
+elif (pontuacao_2 < pontuacao_3 and pontuacao_2 > pontuacao_1):
+    pontuacao_segundo = pontuacao_2
+    nome_segundo = nome_2
+elif (pontuacao_3 < pontuacao_1 and pontuacao_3 > pontuacao_2):
+    pontuacao_segundo = pontuacao_2
+    nome_segundo = nome_2
+elif (pontuacao_3 < pontuacao_2 and pontuacao_3 > pontuacao_1):
+    pontuacao_segundo = pontuacao_3
+    nome_segundo = nome_3
+elif (pontuacao_1 == pontuacao_2 == pontuacao_3):
+    if (nome_1 < nome_2 and nome_1 > nome_3):
+        pontuacao_segundo = pontuacao_1
+        nome_segundo = nome_1
+    elif (nome_1 < nome_3 and nome_1 > nome_2):
+        pontuacao_segundo = pontuacao_1
+        nome_segundo = nome_1
+    elif (nome_2 < nome_1 and nome_2 > nome_3):
+        pontuacao_segundo = pontuacao_2
+        nome_segundo = nome_2
+    elif (nome_2 < nome_3 and nome_2 > nome_1):
+        pontuacao_segundo = pontuacao_2
+        nome_segundo = nome_2
+    elif (nome_3 < nome_1 and nome_3 > nome_2):
+        pontuacao_segundo = pontuacao_3
+        nome_segundo = nome_3
+    elif (nome_3 < nome_2 and nome_3 > nome_2):
+        pontuacao_segundo = pontuacao_3
+        nome_segundo = nome_3
+elif (pontuacao_1 == pontuacao_2 and pontuacao_1 > pontuacao_3):
+    if (pontuacao_1 < pontuacao_2):
+        pontuacao_segundo = pontuacao_1
+        nome_segundo = nome_1
+    else:
+        pontuacao_segundo = pontuacao_2
+        nome_segundo = nome_2
+elif (pontuacao_2 == pontuacao_3 and pontuacao_2 > pontuacao_1):
+    if (pontuacao_2 < pontuacao_3):
+        pontuacao_segundo = pontuacao_2
+        nome_segundo = nome_2
+    else:
+        pontuacao_segundo = pontuacao_3
+        nome_segundo = nome_3
+elif (pontuacao_1 == pontuacao_3 and pontuacao_1 > pontuacao_2):
+    if (pontuacao_1 < pontuacao_3):
+        pontuacao_segundo = pontuacao_1
+        nome_segundo = nome_1
+    else:
+        pontuacao_segundo = pontuacao_3
+        nome_segundo = nome_3
+
+#Terceiro lugar
+if (pontuacao_1 > pontuacao_2 and pontuacao_1 > pontuacao_3):
+    nome_terceiro = nome_1
+    pontuacao_terceiro = pontuacao_1
+elif (pontuacao_2 > pontuacao_3):
+    nome_terceiro = nome_2
+    pontuacao_terceiro = pontuacao_2
+elif (pontuacao_1 == pontuacao_2 == pontuacao_3):
+    if (nome_1 > nome_2 and nome_1 > nome_3):
+        nome_terceiro = nome_1
+        pontuacao_terceiro = pontuacao_1
+    elif (nome_2 > nome_3):
+        nome_terceiro = nome_2
+        pontuacao_terceiro = pontuacao_2
+    else:
+        nome_terceiro = nome_3
+        pontuacao_terceiro = pontuacao_3
+elif (pontuacao_1 == pontuacao_2 and pontuacao_1 > pontuacao_3):
+    if (nome_1 > nome_2):
+        nome_terceiro = nome_1
+        pontuacao_terceiro = pontuacao_1
+    else:
+        nome_terceiro = nome_2
+        pontuacao_terceiro = pontuacao_2
+elif (pontuacao_2 == pontuacao_3 and pontuacao_2 > pontuacao_1):
+    if (nome_2 > nome_3):
+        nome_terceiro = nome_2
+        pontuacao_terceiro = pontuacao_2
+    else:
+        nome_terceiro = nome_3
+        pontuacao_terceiro = pontuacao_3
+elif (pontuacao_1 == pontuacao_3 and pontuacao_1 > pontuacao_2):
+    if (nome_1 > nome_3):
+        nome_terceiro = nome_1
+        pontuacao_terceiro = pontuacao_1
+    else:
+        nome_terceiro = nome_3
+        pontuacao_terceiro = pontuacao_3
+else:
+    nome_terceiro = nome_3
+    pontuacao_terceiro = pontuacao_3
+
+#Resultados
+print(nome_primeiro, pontuacao_primeiro)
+print(nome_segundo, pontuacao_segundo)
+print(nome_terceiro, pontuacao_terceiro)
