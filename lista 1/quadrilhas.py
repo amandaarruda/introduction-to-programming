46bcfb4fe98f3b51f64180e7c8b6644b69bd2674
Amanda Arruda <66084295+amandaarruda@users.noreply.github.com>
2022-07-11 09:05:54 -0300

    bugfix

--- lista 1/quadrilhas.py
+++ lista 1/quadrilhas.py
@@ -1,6 +1,15 @@
 #Recebendo o nome da quadrilha
 nome_da_quadrilha = input()
 
+#Passos permitidos
+passo_A = "Cumprimento"
+passo_B = "Balancê"
+passo_C = "Passeio"
+passo_D = "Túnel"
+passo_E = "Serrote"
+passo_F = "Casamento"
+passo_G = "Despedida"
+
 #Recebendo os passos
 passo_1 = input()
 passo_2 = input()
@@ -8,52 +17,169 @@ passo_3 = input()
 passo_4 = input()
 passo_5 = input()
 
-passos = passo_1 or passo_2 or passo_3 or passo_4 or passo_5
+#Definindo a pontuação 
+pontuacao = float(0)
 
-#definindo a pontuação 
-pontuacao = 0
+#Declarando o casamento e os passos diferentes
+casamento = False
+diferente = False
 
 #Regras
 #1
 #Deve ter 5 passos
 conjunto_passos = passo_1 + passo_2 + passo_3 + passo_4 + passo_5
-#2
-#Caso algum passo seja diferente
-if (passos) != ("Cumprimento" or "Balancê" or "Passeio" or "Túnel" or "Serrote" or "Casamento" or "Despedida"):
+#Regras que dependem de cada passo:
+#Passo 1
+#Regra do Cumprimento
+if (passo_1 == passo_A):
+    pontuacao = 100
+#Regra do Balancê
+elif (passo_1 == passo_B):
+    pontuacao = 50
+#Regra do Passeio
+elif (passo_1 == passo_C):
+    pontuacao = 70
+#Regra do Serrote
+elif (passo_1 == passo_E):
+    pontuacao = 100
+#Regra da Despedida
+elif (passo_1 == passo_G):
     pontuacao = 0
-#3
-#Cumprimento no início
-if (passo_1 == "Cumprimento"):
-    pontuacao_1 = pontuacao + 100
-elif (passo_2 or passo_3 or passo_4 or passo_5 == "Cumprimento"):
-    pontuacao_1 = pontuacao + 10
-#4
-if (passos == "Balancê"):
-    teste = pontuacao + 50
-#5
-if (passos == "Passeio"):
-    pontuacao_3 = pontuacao + 70
-#6
-if (passos == "Serrote"):
-    pontuacao_4 = pontuacao + 100
-#7
-if (passo_5 == "Despedida"):
-    pontuacao_5 = pontuacao + 35
-#8
-if (passos == "Túnel"):
-    pontuacao_6 = pontuacao * 0.9
-#9 
-if (passos == "Casamento"):
-    pontuacao_7 = pontuacao * 2
+#Regra do Túnel
+elif (passo_1 == passo_D):
+    pontuacao = pontuacao * 0.9
+#Regra do Casamento
+elif (passo_1 == passo_F):
+    casamento = True
+#Passo diferente
+elif (passo_1 != passo_A, passo_B, passo_C, passo_D, passo_E, passo_F, passo_G):
+    diferente = True
+else:
+    pontuacao = 0
 
-#Definindo a pontuação
-pontuacao = pontuacao_1 + teste + pontuacao_3 + pontuacao_4 + pontuacao_5 + pontuacao_6 + pontuacao_7
+#Passo 2
+#Regra do Cumprimento
+if (passo_2 == passo_A):
+    pontuacao += 10
+#Regra do Balancê
+elif (passo_2 == passo_B):
+    pontuacao += 50
+#Regra do Passeio
+elif (passo_2 == passo_C):
+    pontuacao += 70
+#Regra do Serrote
+elif (passo_2 == passo_E):
+    pontuacao += 100
+#Regra da Despedida
+elif (passo_2 == passo_G):
+    pontuacao = pontuacao
+#Regra do Túnel
+elif (passo_2 == passo_D):
+    pontuacao *= 0.9
+#Regra do Casamento
+elif (passo_2 == passo_F):
+    casamento = True
+#Passo diferente
+elif (passo_2 != passo_A, passo_B, passo_C, passo_D, passo_E, passo_F, passo_G):
+    diferente = True
+else:
+    pontuacao = 0
+
+#Passo 3
+#Regra do Cumprimento
+if (passo_3 == passo_A):
+    pontuacao += 10
+#Regra do Balancê
+elif (passo_3 == passo_B):
+    pontuacao += 50
+#Regra do Passeio
+elif (passo_3 == passo_C):
+    pontuacao += 70
+#Regra do Serrote
+elif (passo_3 == passo_E):
+    pontuacao += 100
+#Regra da Despedida
+elif (passo_3 == passo_G):
+    pontuacao = pontuacao
+#Regra do Túnel
+elif (passo_3 == passo_D):
+    pontuacao *= 0.9
+#Regra do Casamento
+elif (passo_3 == passo_F):
+    casamento = True
+#Passo diferente
+elif (passo_3 != passo_A, passo_B, passo_C, passo_D, passo_E, passo_F, passo_G):
+    diferente = True
+else:
+    pontuacao = 0
+
+#Passo 4
+#Regra do Cumprimento
+if (passo_4 == passo_A):
+    pontuacao += 10
+#Regra do Balancê
+elif (passo_4 == passo_B):
+    pontuacao += 50
+#Regra do Passeio
+elif (passo_4 == passo_C):
+    pontuacao += 70
+#Regra do Serrote
+elif (passo_4 == passo_E):
+    pontuacao += 100
+#Regra da Despedida
+elif (passo_4 == passo_G):
+    pontuacao = pontuacao
+#Regra do Túnel
+elif (passo_4 == passo_D):
+    pontuacao *= 0.9
+#Regra do Casamento
+elif (passo_4 == passo_F):
+    casamento = True
+#Passo diferente
+elif (passo_4 != passo_A, passo_B, passo_C, passo_D, passo_E, passo_F, passo_G):
+    diferente = True
+else:
+    pontuacao = 0
+
+#Passo 5
+#Regra do Cumprimento
+if (passo_5 == passo_A):
+    pontuacao += 10
+#Regra do Balancê
+elif (passo_5 == passo_B):
+    pontuacao += 50
+#Regra do Passeio
+elif (passo_5 == passo_C):
+    pontuacao += 70
+#Regra do Serrote
+elif (passo_5 == passo_E):
+    pontuacao += 100
+#Regra da Despedida
+elif (passo_5 == passo_G):
+    pontuacao += 35
+#Regra do Túnel
+elif (passo_5 == passo_D):
+    pontuacao *= 0.9
+#Regra do Casamento
+elif (passo_5 == passo_F):
+    casamento = True
+#Passo diferente
+elif (passo_5 != passo_A, passo_B, passo_C, passo_D, passo_E, passo_F, passo_G):
+    diferente = True
+else:
+    pontuacao = 0
+
+
+#Continuando a regra do Casamento
+if (casamento == True):
+    pontuacao = pontuacao * 2
+
 
 #Resultados
-if (pontuacao == 0):
-    print(f'Poxa, {nome_da_quadrilha}. Por utilizarem de algum passo não permitido vocês tiveram a pontuação zerada')
+if (diferente == True):
+    print(f'Poxa, {nome_da_quadrilha}. Por utilizarem de algum passo não permitido vocês tiveram a pontuação zerada.')
 elif (pontuacao > 0):
     def conjunto_passos():
         return True
     if conjunto_passos():
-            print(f'Parabéns, {nome_da_quadrilha}! Bela apresentação. A pontuação foi de {pontuacao}!')
+            print(f"Parabéns, {nome_da_quadrilha}! Bela apresentação. A pontuação foi de {pontuacao:.1f}!")
