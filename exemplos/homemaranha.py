f8d64857ba50a1389dc1d14b708f6e9a653cfa75
Amanda Arruda <66084295+amandaarruda@users.noreply.github.com>
2022-07-15 08:16:00 -0300

    Create homemaranha.py

---
+++ exemplos/homemaranha.py
@@ -0,0 +1,22 @@
+nome1 = input()
+poder1 = int(input())
+local1 = int(input())
+
+nome2 = input()
+poder2 = int(input())
+local2 = int(input())
+
+destruicao1 = (poder1**2 * local1)/2
+destruicao2 = (poder2**2 * local2)/2
+
+if(poder1 < 0 or poder2 < 0):
+  print("Nem faco essa comparacao! Quero ser um vingador, busco coisas maiores.")
+elif(destruicao1 > destruicao2):
+  print(f'Com grandes poderes, vem grandes responsabilidades. Com isso, preciso ir deter o {nome1}.')
+elif(destruicao1 < destruicao2):
+  print(f'Com grandes poderes, vem grandes responsabilidades. Com isso, preciso ir deter o {nome2}.')
+else:
+  if (destruicao1 % 2 == 0):
+    print('E quem disse que isso e problema meu? Vou chamar o senhor Stark.')
+  else:
+    print('Vou chamar uns reforcos de outro universo... rsrs')
