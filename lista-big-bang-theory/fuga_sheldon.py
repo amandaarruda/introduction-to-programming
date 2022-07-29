c985c2ce7720740f0f6c153a7a7fb8397fcaaed5
Amanda Arruda <66084295+amandaarruda@users.noreply.github.com>
2022-07-29 08:28:54 -0300

    Update fuga_sheldon.py

--- lista-big-bang-theory/fuga_sheldon.py
+++ lista-big-bang-theory/fuga_sheldon.py
@@ -1,16 +1,39 @@
 #planetas a serem comparados
 quantidade_planetas = int(input())
 
-for i in range(quantidade_planetas):
-    
-    if (quantidade_planetas < 2):
-        print('Número inválido, tente novamente')
-        quantidade_planetas = int(input())
-    else:
-        nome = input()
-        raio = float(input())
-        massa = float(input())
-        temperatura = int(input())
+while quantidade_planetas < 2:   
+    print('Número inválido, tente novamente')
+    quantidade_planetas = int(input())
 
-        indice = ((raio + massa + (temperatura/288))/3)
-    indice 
+#Variáveis para ajudar na comparação
+indice_passado = float('inf')
+nome_passado = ''
+melhor_planeta = ''
+melhor_indice = 0
+
+for i in range(quantidade_planetas):
+    nome = input()
+    raio = float(input())
+    massa = float(input())
+    temperatura = int(input())
+
+    indice = ((raio + massa + (temperatura/288))/3)
+
+    if (abs(1-indice) < abs(1-indice_passado)):
+        melhor_planeta = nome
+        melhor_indice = indice
+    else:
+        melhor_planeta = nome_passado
+        melhor_indice = indice_passado
+
+    #Salvando para a próxima comparação
+    indice_passado = melhor_indice
+    nome_passado = melhor_planeta
+
+#Resultados
+if (melhor_indice == 1):
+    print(f'{melhor_planeta} é perfeito!')
+elif (melhor_indice < 1):
+    print(f'{melhor_planeta} vai dar pro gasto')
+elif (melhor_indice > 1):
+    print(f'{melhor_planeta} vai ter que servir')
