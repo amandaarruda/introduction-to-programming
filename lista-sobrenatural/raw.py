4373601fb7fe1fef713864fef91500f035bf950b
Amanda Arruda <66084295+amandaarruda@users.noreply.github.com>
2022-08-18 10:59:45 -0300

    Update raw.py

--- lista-sobrenatural/raw.py
+++ lista-sobrenatural/raw.py
@@ -20,26 +20,35 @@ for i in range(tamanho): #sendo i um elemento da linha
         if(i == j):
             diagonal_principal.append(matriz[i][j])
 
-for l in range(tamanho - 1):
-    for c in range(tamanho - 1):
-        soma = matriz[l][c] + matriz[l][c+1] #fazendo a soma horizontal
-        somas_linhas.append(soma)
-        soma = matriz[c][l] + matriz[c+1][l] #fazendo a soma vertical
-        somas_colunas.append(soma)
+for l in range(tamanho):
+    for c in range(tamanho):
+        if (l < tamanho-1):
+            soma = matriz[l][c] + matriz[l][c+1] #fazendo a soma horizontal
+            somas_linhas.append(soma)
+        if (c < tamanho-1):
+            soma = matriz[c][l] + matriz[c+1][l] #fazendo a soma vertical
+            somas_colunas.append(soma)
 
+print(somas_linhas, somas_colunas)
+
+for n in range(len(diagonal_principal)-1): #fazendo a soma diagonal
+    soma = diagonal_principal[n] + diagonal_principal[n+1]
+    somas_diagonal_principal.append(soma)
+  
 for somas in somas_linhas: #definindo a maior soma das linhas
-    if (somas > maior_soma_l):
-        maior_soma_l = soma
+    if (int(somas) > int(maior_soma_l)):
+        maior_soma_l = somas
 maiores_somas.append(maior_soma_l)
 
 for somas in somas_colunas: #definindo a maior soma das colunas
-    if (somas > maior_soma_c):
-        maior_soma_c = soma
+    if (int(somas) > int(maior_soma_c)):
+        maior_soma_c = somas
 maiores_somas.append(maior_soma_c)
 
 for somas in somas_diagonal_principal: #definindo a maior soma da diagonal
-    if (somas > maior_soma_d):
-        maior_soma_d = soma
+    if (int(somas) > int(maior_soma_d)):
+        maior_soma_d = somas
 maiores_somas.append(maior_soma_d)
 
-password = maiores_somas.join()
+print(somas_linhas, somas_colunas, somas_diagonal_principal)
+print(maiores_somas)
