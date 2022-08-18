b17c4b2964fc94f6844f6714a87dca008684737c
Amanda Arruda <66084295+amandaarruda@users.noreply.github.com>
2022-08-18 14:53:05 -0300

    Update raw.py

--- lista-sobrenatural/raw.py
+++ lista-sobrenatural/raw.py
@@ -22,14 +22,11 @@ for i in range(tamanho): #sendo i um elemento da linha
 
 for l in range(tamanho):
     for c in range(tamanho):
-        if (l < tamanho-1):
+        if (l < (tamanho - 1) and c < (tamanho - 1)):
             soma = matriz[l][c] + matriz[l][c+1] #fazendo a soma horizontal
             somas_linhas.append(soma)
-        if (c < tamanho-1):
             soma = matriz[c][l] + matriz[c+1][l] #fazendo a soma vertical
             somas_colunas.append(soma)
-
-print(somas_linhas, somas_colunas)
 
 for n in range(len(diagonal_principal)-1): #fazendo a soma diagonal
     soma = diagonal_principal[n] + diagonal_principal[n+1]
@@ -49,6 +46,3 @@ for somas in somas_diagonal_principal: #definindo a maior soma da diagonal
     if (int(somas) > int(maior_soma_d)):
         maior_soma_d = somas
 maiores_somas.append(maior_soma_d)
-
-print(somas_linhas, somas_colunas, somas_diagonal_principal)
-print(maiores_somas)
