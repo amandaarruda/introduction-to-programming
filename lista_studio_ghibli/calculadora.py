0baea0e2c957592dfcc43ce1fd92d68fa3fe1218
Amanda Arruda <66084295+amandaarruda@users.noreply.github.com>
2022-08-22 07:34:00 -0300

    calculadora

--- lista_studio_ghibli/calculadora.py
+++ lista_studio_ghibli/calculadora.py
@@ -1,38 +1,29 @@
-def calculadora(operacao, quantidade_numeros):
+end = False
+valores = [] #armazena os valores a serem trabalhados
 
-    novo_valor_soma = 0
-    novo_valor_mult = 1
-
-    for quantidade in range(quantidade_numeros):
-        valor = int(input())
-
+def calculadora(operacao):
+    for quantidade in range(quantidade_numeros - 1): #-1 pra iteração bater certinho
         if (operacao == 'S'):
-            novo_valor_soma = novo_valor_soma + valor
+            resultado = valores[0] + valores[1]
         elif (operacao == 'sub'):
-            if (quantidade == 0):
-                proximo_valor = int(input())
-                novo_valor_soma = valor - proximo_valor
-            else:
-                novo_valor_soma = novo_valor_soma - valor
+            resultado = valores[0] - valores[1]
         elif (operacao == 'M'):
-            novo_valor_mult = novo_valor_mult * valor
+            resultado = valores [0] * valores[1]
         else:
-            if (quantidade == 0):
-                proximo_valor = float(input())
-                novo_valor_mult = float(valor) / proximo_valor
-            else:
-                novo_valor_mult = novo_valor_mult / float(valor)
+            resultado = float(valores[0]) / valores[1]
+        valores.pop(0) #atualiza o valor
+        valores[0] = resultado
+    
 
-    if (operacao == 'S' or operacao == 'sub'):
-        print(f'{novo_valor_soma}')
-    else:
-        print(f'{novo_valor_mult}')                  
-
-nova_operacao = '1'
-
-while (nova_operacao == '1'):
+while (not end):
     operacao = input()
     quantidade_numeros = int(input())
-    calculadora(operacao, quantidade_numeros)
-
-    nova_operacao = input()
+    for quantidade in range(quantidade_numeros):
+        valor = int(input())
+        valores.append(valor)
+    calculadora(operacao)
+    print(*valores)
+    valores.pop(0) #limpa a lista
+    nova_operacao = int(input())
+    if (nova_operacao != 1):
+        end = True
