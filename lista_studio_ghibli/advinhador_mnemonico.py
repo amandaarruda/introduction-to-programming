818387b354734cba740f1e2ff1e86b4f9164d98a
Amanda Arruda <66084295+amandaarruda@users.noreply.github.com>
2022-08-26 09:25:48 -0300

    calculador de gorjetas por serviço de chihiro

--- lista_studio_ghibli/advinhador_mnemonico.py
+++ lista_studio_ghibli/advinhador_mnemonico.py
@@ -1,53 +1,62 @@
-def separador_silabas(palavra):
-    palavra_separada = [] #reseta a palavra anterior
-    silaba_atual = [] #representa a sílaba que vai ser formada
-    vogais = ['a', 'e', 'i', 'o', 'u']
+def pedras_quentes(valores):
+    valores[2] += ((valores[1] + valores[2]) // 2) #muda a gorjeta
+    valores[0] -= 20 #subtrai do tempo total
+    valores[1] += 20 #atualiza o tempo decorrido
 
-    for letra in palavra:
-        silaba_atual.append(letra) #vai adicionando cada letra na sílaba atual
-        if letra in vogais:
-            silaba_final = ('').join(silaba_atual) #se a letra for uma vogal, a sílaba acaba ali 
-            palavra_separada.append(silaba_final) #adiciona a sílaba dividida
-            silaba_atual = [] #reseta a sílaba atual
+def massagem_pes(valores):
+    if (valores[2] % 2 == 0): #muda a gorjeta
+        valores[2] += ((valores[2] % 7) * 6)
+    else:
+        valores[2] += ((valores[2] % 7)**2)
+    valores[0] -= 30 #subtrai do tempo total
+    valores[1] += 30 #atualiza o tempo decorrido
+        
+def servir_refeicao(valores):
+    if (valores[2] % 10 == 0): #muda a gorjeta
+        valores[2] += 5
+    else:
+        while (valores[2] % 10 != 0): #muda a gorjeta
+            valores[2] += 1
+    valores[0] -= 15 #subtrai do tempo total
+    valores[1] += 15 #atualiza o tempo decorrido
 
-    checador_mnemonico(palavra_separada)
+def massagem_completa(valores):
+    valores[2] = str(valores[2]) 
+    soma = 0
+    for numero in valores[2]: #muda a gorjeta
+        soma += int(numero)
+    valores[2] = int(valores[2]) + soma
+    valores[0] -= 50 #subtrai do tempo total
+    valores[1] += 50 #atualiza o tempo decorrido
 
-def checador_mnemonico(palavra_separada):
-    ordem_formada = [] #é a ordem das sílabas que estão na palavra e no nome do hospital
-    for silaba in palavra_separada:
-        if (silaba in hospital):
-            ordem_formada.append(silaba)
-            if (silaba not in silabas_encontradas):
-                silabas_encontradas.append(silaba)
-            else:
-                ordem_formada.remove(silaba)
+valores = [120, 0, 10] #onde valores[0] é o tempo total, valores[1] é o tempo decorrido e valores[2] é o total de gorjetas
+servico_inexistente = False
 
-    resposta(ordem_formada, palavra_separada)
+while (valores[0] > 0 and valores[2] < 50):
+    servico = input()
+    servico_inexistente = False
+    if (servico == 'pedras'):
+        pedras_quentes(valores)
+        nome_servico = 'Pedras Quentes'
+    elif (servico == 'pes'):
+        massagem_pes(valores)
+        nome_servico = 'Massagem nos Pes'
+    elif (servico == 'refeicao'):
+        servir_refeicao(valores)
+        nome_servico = 'Servir Refeicao'
+    elif (servico == 'completa'):
+        massagem_completa(valores)
+        nome_servico = 'Massagem Completa'
+    else:
+        servico_inexistente = True
+        valores[0] -= 5 #subtrai do tempo
+        valores[1] += 5 #atualiza o tempo decorrido
+        print(f'O cliente fez voce perder tempo, voce ainda possui {valores[2]} gorjetas.')
 
-def resposta(ordem_formada, palavra_separada):
-    if (len(ordem_formada) == 0):
-        print('Infelizmente nenhuma dessas sílabas me lembrou do nome do hospital, Totoro.')
-    elif (len(ordem_formada) == 1):
-        print(f'Lembrei! A sílaba {ordem_formada[0]} está no nome do hospital. Obrigada, Totoro!')
-    elif (len(ordem_formada) > 1):
-        if (palavra_separada == ordem_formada):
-            if (palavra in palavras_embutidas_hospital):
-                print(f'A palavra {palavra} está toda no nome do hospital. Acertou em cheio, Totoro!')
-            else:
-                silabas = (', ').join(ordem_formada)
-                print(f'Lembrei! As sílabas: {silabas} estão no nome do hospital. Obrigada, Totoro!')        
-        else:
-            silabas = (', ').join(ordem_formada)
-            print(f'Lembrei! As sílabas: {silabas} estão no nome do hospital. Obrigada, Totoro!')
-
-hospital = ['shi', 'chi', 'ko', 'ku', 'ya', 'ma']
-palavras_embutidas_hospital = ['shichi', 'chiko', 'koku', 'kuya', 'yama', 'shichiko', 'chikoku', 'kokuya', 'kuyama', 
-'shichikoku', 'chikokuya', 'kokuyama', 'shichikokuya', 'chikokuyama','shichikokuyama']
-silabas_encontradas = [] #guarda todas as sílabas corretas até o momento
-ordem_formada = []
-
-while (len(silabas_encontradas) != 6): #enquanto não tivermos achado todas as sílabas
-    palavra = input()
-    separador_silabas(palavra)
+    if (not servico_inexistente):
+        print(f'Voce concluiu o servico de {nome_servico} e agora possui {valores[2]} gorjetas.')
 else:
-    print('Conseguimos lembrar o nome do hospital shichikokuyama, agora é só pegar o Catbus e ir até lá!')
+    if (valores[2] >= 50):
+        print(f'Você acumulou {valores[2]} gorjetas, com essa quantidade voce conseguira comprar sua passagem de saida e salvar seus pais.')
+    else:
+        print('Voce nao conseguiu o minimo necessario para comprar a passagem de saida desse mundo e salvar seus pais.')
