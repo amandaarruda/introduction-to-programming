0b149243521761ec90b76dcb12635778f4c4cee4
Amanda Arruda <66084295+amandaarruda@users.noreply.github.com>
2022-09-21 07:49:08 -0300

    Update target.py

--- dictionaries-&-tuples;/target.py
+++ dictionaries-&-tuples;/target.py
@@ -1,23 +1,24 @@
 jogadores = {} #aqui as keys vão ser o "index" da posição dos jogadores
 
-posicoes = list(input())
+posicoes = input()
 alvo = int(input())
 
-for elemento in posicoes: #limpando a lista
-    if elemento == '[' or elemento == ']' or elemento == ',':
-        posicoes.remove(elemento)
+posicoes = posicoes.replace('[', '') #removendo o que não é número
+posicoes = posicoes.replace(']', '')
+posicoes = [int(numero) for numero in posicoes.split(',')]
 
 for index in range(len(posicoes)): #pego os indexes da lista
     jogadores[index] = int(posicoes[index])  #transformo a key no index e relaciono com o número da lista
 
-for index in range(len(posicoes)):
-    if jogadores.get(index) + int(posicoes[index]) == alvo:
-        if jogadores.get(index) < int(posicoes[index]):
-            jogador_1 = jogadores.get(index)
-            jogador_2 = index
-        else:
-            jogador_2 = jogadores.get(index)
-            jogador_1 = index
-        resposta = [jogador_1, jogador_2]
-
-print(resposta)
+for jogador in jogadores.keys():
+    for outro_jogador in jogadores.keys():
+        if outro_jogador != jogador: #para não ser o mesmo
+            if jogadores[jogador] + jogadores[outro_jogador] == alvo: #se a soma deles for o alvo
+                if jogadores[jogador] < jogadores[outro_jogador]:
+                    jogador_1 = jogador
+                    jogador_2 = outro_jogador
+                else:
+                    jogador_1 = outro_jogador
+                    jogador_2 = jogador
+                
+print([jogador_1, jogador_2])
